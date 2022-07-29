e48e07db32608ae64844853820b5f99a3cd0b142
alexhou00 <alexhou00@gmail.com>
2022-07-29 17:46:39 +0800

    Update 20220729 Problem solved

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def upload_file():
             # print(type(file))
             image_file = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             image_file_resize = cv2.resize(image_file, (128, 128), interpolation=cv2.INTER_AREA)
+            image_file_resize = image_file_resize.astype('float32') / 255
             # plt.imshow(image_file_resize)
             # print(type(image_file_resize))
         else:
@@ -65,13 +66,15 @@ def upload_file():
         test_pred_p_arg = np.argsort(test_pred_p)
         test_pred_p_arg = test_pred_p_arg[:, ::-1]
         test_pred = np.argmax(test_pred, axis=1)
-        string = ""
+        string = "Showing the last uploaded file<br>" * (len(files) > 1)
         # test_label_1 = np.argmax(test_label, axis=1)
 
         display_labels = ['0 text', '1 scenic', '2 city', '3 admin', '4 star',
                           '5 photo', '6 human', '7 object', '8 bldg', '9 ritual']
         for i, j in zip(test_pred_p_arg[0], test_pred_p_sorted[0]):
-            string += ((display_labels[i].ljust(10)+(str(j)+'%').rjust(4))+'<br>').replace(' ', '&nbsp;')
+            if j:
+                string += ((display_labels[i].ljust(10)+(str(j)+'%').rjust(4))+'<br>')\
+                            .replace(' ', '&nbsp;')
 
     if success and errors:
         errors['message'] = string  # 'File successfully uploaded<br>'+string
@@ -90,21 +93,3 @@ def upload_file():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
-
-
-'''
-# Test
-test_pred = model.predict(np.array([test_image[0]]), verbose=1)
-test_pred_p = test_pred * 100
-test_pred_p = test_pred_p.astype('int32')
-test_pred_p_sorted = np.sort(test_pred_p)
-test_pred_p_sorted = test_pred_p_sorted[:,::-1]
-test_pred_p_arg = np.argsort(test_pred_p)
-test_pred_p_arg = test_pred_p_arg[:,::-1]
-test_pred = np.argmax(test_pred, axis=1)
-test_label_1 = np.argmax(test_label, axis=1)
-display_labels = ['0 text', '1 scenic', '2 city', '3 admin', '4 star',
-                  '5 photo', '6 human', '7 object', '8 bldg', '9 ritual']
-for i, j in zip(test_pred_p_arg[0], test_pred_p_sorted[0]):
-    print(display_labels[i].ljust(10)+(str(j)+'%').rjust(4))
-'''
